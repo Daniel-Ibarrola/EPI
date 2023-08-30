71624df4a890b2baa64f7b6a40972a2788057433
Daniel Ibarrola <dis96051@gmail.com>
2023-08-30 08:49:15 -0600

    Binay search with duplicates

--- src/searching/search.h
+++ src/searching/search.h
@@ -28,4 +28,38 @@ int binarySearch(const std::vector<data_type>& array, data_type element){
     return -1;
 }
 
+
+template <typename data_type>
+int binarySearchDup(const std::vector<data_type>& array, data_type key){
+    // Binary search that can handle repeated keys. If a key is repeated
+    // returns the index of the first occurrence. Returns -1 if key is not
+    // found
+    if (array.empty())
+        return -1;
+
+    int firstOccurrence {-1};
+    int left {0};
+    int right {array.size() - 1};
+
+    while (left <= right) {
+        int mid {left + (right - left) / 2};
+        if (array[mid] == key){
+            firstOccurrence = static_cast<int>(mid);
+            right = mid - 1;
+        }
+        else if (array[mid] < key)
+            left = mid + 1;
+        else
+            right = mid - 1;
+    }
+
+    return firstOccurrence;
+}
+
+
+template <typename data_type>
+int firstLarger(const std::vector<data_type>& array, data_type key){
+    return -1;
+}
+
 #endif //INTERVIEWPREP_SEARCH_H
--- tests/searching/test_binary_search.cpp
+++ tests/searching/test_binary_search.cpp
@@ -12,10 +12,17 @@ TEST(TestBinarySearch, EmptyVector)
 }
 
 
-TEST(TestBinarySearch, FindsElementIfPresent)
+TEST(TestBinarySearch, FindsElementIfPresent1)
 {
     std::vector<int> arr {1, 2, 6, 8, 9};
     ASSERT_EQ(binarySearch(arr, 8), 3);
+}
+
+
+TEST(TestBinarySearch, FindsElementIfPresent2)
+{
+    std::vector<int> arr {1, 2, 6, 8, 9};
+    ASSERT_EQ(binarySearch(arr, 1), 0);
 }
 
 
@@ -30,4 +37,60 @@ TEST(TestBinarySearch, RepeatedElement)
 {
     std::vector<int> arr {1, 2, 2, 6, 8, 9};
     ASSERT_EQ(binarySearch(arr, 2), 2);
+}
+
+
+TEST(TestBinarySearchWithDuplicates, ElementNotFound)
+{
+    std::vector<int> arr {-14, -10, 2, 108, 108, 243, 285, 285, 285, 401};
+    ASSERT_EQ(binarySearchDup(arr, 250), -1);
+}
+
+
+TEST(TestBinarySearchWithDuplicates, ReturnsFirstOccurrence1)
+{
+    std::vector<int> arr {-14, -10, 2, 108, 108, 243, 285, 285, 285, 401};
+    ASSERT_EQ(binarySearchDup(arr, 108), 3);
+}
+
+
+TEST(TestBinarySearchWithDuplicates, ReturnsFirstOccurrence2)
+{
+    std::vector<int> arr {-14, -10, 2, 108, 108, 243, 285, 285, 285, 401};
+    ASSERT_EQ(binarySearchDup(arr, 285), 6);
+}
+
+
+TEST(TestBinarySearchWithDuplicates, AllTheSame)
+{
+    std::vector<int> arr {2, 2, 2, 2, 2};
+    ASSERT_EQ(binarySearchDup(arr, 2), 0);
+}
+
+
+TEST(TestFirstLarger, NoLargerElement1)
+{
+    std::vector<int> arr {-14, -10, 2, 108, 108, 243, 285, 285, 285, 401};
+    ASSERT_EQ(firstLarger(arr, 500), -1);
+}
+
+
+TEST(TestFirstLarger, NoLargerElement2)
+{
+    std::vector<int> arr {-14, -10, 2, 108, 108, 243, 285, 285, 401, 401};
+    ASSERT_EQ(firstLarger(arr, 401), -1);
+}
+
+
+TEST(TestFirstLarger, FindsLargerElment1)
+{
+    std::vector<int> arr {-14, -10, 2, 108, 108, 243, 285, 285, 285, 401};
+    ASSERT_EQ(firstLarger(arr, 101), 3);  // arr[3] = 108
+}
+
+
+TEST(TestFirstLarger, FindsLargerElment2)
+{
+    std::vector<int> arr {-14, -10, 2, 108, 108, 243, 285, 285, 285, 401};
+    ASSERT_EQ(firstLarger(arr, 250), 6);  // arr[6] = 285
 }
